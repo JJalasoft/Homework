a4987381ec882d746cd9defad463a7f4eca97856
JJalasoft <111385139+JJalasoft@users.noreply.github.com>
2022-08-17 13:57:42 -0700

    Update HW_2.py

--- HW_2.py
+++ HW_2.py
@@ -4,11 +4,12 @@ import string
 
 #terminal flags
 parser = argparse.ArgumentParser(description = "Count words in text")
-parser.add_argument('-w', '--word', type=str, help='Receive a word to count it in the text file')
+parser.add_argument('-w', '--word', type=str, help='Receive a word to search it in the text file')
 args = parser.parse_args()
 
 words_to_count = args.word
 #---------------------
+
 filename = "romeo.txt"
 
 def remove_puncts_from_file(filename):
@@ -31,6 +32,9 @@ def words_counting(text):
         words_count[unrepeated] = counter
     return words_count
 
+
 text = remove_puncts_from_file(filename)
-word_count = words_counting(text)
-print(word_count[str(words_to_count)])
+counted_words = words_counting(text)
+#print(counted_words)
+if str(words_to_count) in counted_words:
+    print(counted_words[str(words_to_count)])
